5793472d751d34645a98ed692b5be0f04aa38f7f
peter-lyr <llydrp.ldp@gmail.com>
2023-04-19 00:27:03 +0800

    autopairs InsertEnter

--- share/nvim/runtime/pack/my-packer/start/autopairs/plugin/autopairs.lua
+++ share/nvim/runtime/pack/my-packer/start/autopairs/plugin/autopairs.lua
@@ -16,7 +16,7 @@ end
 
 if not g.autopairs_startup then
   g.autopairs_startup = 1
-  g.autopairs_cursormoved = a.nvim_create_autocmd({"CursorMoved"}, {
+  g.autopairs_cursormoved = a.nvim_create_autocmd({"InsertEnter"}, {
     callback = function()
       a.nvim_del_autocmd(g.autopairs_cursormoved)
       autopairs()
