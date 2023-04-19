57ee42c47a14ea7c71fba9006a6e6eed5cfc4d72
peter-lyr <llydrp.ldp@gmail.com>
2023-04-20 00:06:25 +0800

    diffview

---
+++ share/nvim/runtime/pack/my-packer/start/diffview/plugin/diffview.lua
@@ -0,0 +1,39 @@
+local a = vim.api
+local f = vim.fn
+local g = vim.g
+local s = vim.keymap.set
+
+local sta
+
+local diffview = function(params)
+  if not g.diffview_loaded then
+    g.diffview_loaded = 1
+    if g.diffview_cursormoved then
+      a.nvim_del_autocmd(g.diffview_cursormoved)
+    end
+    sta, Do_diffview = pcall(require, 'do_diffview')
+    if not sta then
+      print(Do_diffview)
+      return
+    end
+  end
+  if not Do_diffview then
+    return
+  end
+  Do_diffview.run(params)
+end
+
+a.nvim_create_user_command('DiffvieW', function(params)
+  diffview(params['fargs'])
+end, { nargs = "*", })
+
+local opt = { silent = true }
+
+
+s({ 'n', 'v' }, '<leader>gi', ':DiffvieW filehistory<cr>', opt)
+s({ 'n', 'v' }, '<leader>go', ':DiffvieW open<cr>', opt)
+s({ 'n', 'v' }, '<leader>gq', ':DiffvieW quit<cr>', opt)
+
+s({ 'n', 'v' }, '<leader>gT', ':DiffvieW toggle_history_cnt<cr>', opt)
+s({ 'n', 'v' }, '<leader>ge', ':DiffvieW DiffviewRefresh<cr>', opt)
+s({ 'n', 'v' }, '<leader>gl', ':DiffvieW DiffviewToggleFiles<cr>', opt)
