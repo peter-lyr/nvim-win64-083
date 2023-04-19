4211bb3f5b0b8196dd98d406151e00fa644534db
peter-lyr <llydrp.ldp@gmail.com>
2023-04-19 22:54:03 +0800

    buffernew顺带把bufferquit也写了

---
+++ share/nvim/runtime/pack/my-packer/start/buffernew/lua/do_buffernew.lua
@@ -0,0 +1,41 @@
+local a = vim.api
+local c = vim.cmd
+
+local M = {}
+
+local open_fpath = function()
+  if M.split == 'up' then
+    c'leftabove split'
+  elseif M.split == 'right' then
+    c'rightbelow vsplit'
+  elseif M.split == 'down' then
+    c'rightbelow split'
+  elseif M.split == 'left' then
+    c'leftabove vsplit'
+  end
+  c('e ' .. M.stack_fpath)
+end
+
+function M.open(mode)
+  M.split = mode
+  open_fpath()
+end
+
+function M.copy_fpath()
+  M.stack_fpath = a['nvim_buf_get_name'](0)
+  print(M.stack_fpath)
+end
+
+M.run = function(params)
+  if not params or #params == 0 then
+    return
+  end
+  local cmd = params[1]
+  if cmd == 'copy_fpath' then
+    M.copy_fpath()
+  else
+    M.open(cmd)
+  end
+end
+
+return M
---
+++ share/nvim/runtime/pack/my-packer/start/buffernew/plugin/buffernew.lua
@@ -0,0 +1,55 @@
+local a = vim.api
+local g = vim.g
+local s = vim.keymap.set
+
+local sta
+
+local buffernew = function(params)
+  if not g.buffernew_loaded then
+    g.buffernew_loaded = 1
+    if g.buffernew_cursormoved then
+      a.nvim_del_autocmd(g.buffernew_cursormoved)
+    end
+    sta, Do_buffernew = pcall(require, 'do_buffernew')
+    if not sta then
+      print(Do_buffernew)
+      return
+    end
+  end
+  if not Do_buffernew then
+    return
+  end
+  Do_buffernew.run(params)
+end
+
+a.nvim_create_user_command('BufferneW', function(params)
+  buffernew(params['fargs'])
+end, { nargs = "*", })
+
+
+local opt = { silent = true }
+
+s({ 'n', 'v' }, '<leader><leader><leader>z', ':BufferneW do<cr>', opt)
+
+s({'n', 'v'}, '<leader>bn', ':leftabove split<cr>', opt)
+s({'n', 'v'}, '<leader>bm', ':leftabove new<cr>', opt)
+s({'n', 'v'}, '<leader>bo', ':leftabove vsplit<cr>', opt)
+s({'n', 'v'}, '<leader>bp', ':leftabove vnew<cr>', opt)
+
+s({'n', 'v'}, '<leader>ba', ':split<cr>', opt)
+s({'n', 'v'}, '<leader>bb', ':new<cr>', opt)
+s({'n', 'v'}, '<leader>bc', ':vsplit<cr>', opt)
+s({'n', 'v'}, '<leader>bd', ':vnew<cr>', opt)
+s({'n', 'v'}, '<leader>be', '<c-w>s<c-w>t', opt)
+s({'n', 'v'}, '<leader>bf', ':tabnew<cr>', opt)
+
+s({'n', 'v'}, '<leader>bg', ':BufferneW copy_fpath<cr>', opt)
+s({'n', 'v'}, '<leader>bi', ':BufferneW here<cr>', opt)
+s({'n', 'v'}, '<leader>bk', ':BufferneW up<cr>', opt)
+s({'n', 'v'}, '<leader>bj', ':BufferneW down<cr>', opt)
+s({'n', 'v'}, '<leader>bh', ':BufferneW left<cr>', opt)
+s({'n', 'v'}, '<leader>bl', ':BufferneW right<cr>', opt)
+
+s({'n', 'v'}, '<leader>x', ':BufferneW copy_fpath<cr>:try|hide|catch|endtry<cr>', opt)
+s({'n', 'v'}, '<a-bs>', ':bw!<cr>', opt)
+s({'n', 'v'}, 'ZX', ':qa!<cr>', opt)
