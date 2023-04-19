abf3cb91928798f197d69b366b9361399b40471f
peter-lyr <llydrp.ldp@gmail.com>
2023-04-20 01:35:51 +0800

    move cursor error

--- share/nvim/runtime/pack/my-packer/opt/maps/plugin/esc.lua
+++ share/nvim/runtime/pack/my-packer/opt/maps/plugin/esc.lua
@@ -1,17 +1,16 @@
 local s = vim.keymap.set
-local o = { silent = true }
 
-s('v', 'm', '<esc>', o)
+s('v', 'm', '<esc>')
 
-s({ 'i', 'c', }, 'ql', '<esc><esc>', o)
-s({ 'i', 'c', }, 'qL', '<esc><esc>', o)
-s({ 'i', 'c', }, 'Ql', '<esc><esc>', o)
-s({ 'i', 'c', }, 'QL', '<esc><esc>', o)
-s('t', 'ql', '<c-\\><c-n>', o)
-s('t', 'qL', '<c-\\><c-n>', o)
-s('t', 'Ql', '<c-\\><c-n>', o)
-s('t', 'QL', '<c-\\><c-n>', o)
+s({ 'i', 'c', }, 'ql', '<esc><esc>')
+s({ 'i', 'c', }, 'qL', '<esc><esc>')
+s({ 'i', 'c', }, 'Ql', '<esc><esc>')
+s({ 'i', 'c', }, 'QL', '<esc><esc>')
+s('t', 'ql', '<c-\\><c-n>')
+s('t', 'qL', '<c-\\><c-n>')
+s('t', 'Ql', '<c-\\><c-n>')
+s('t', 'QL', '<c-\\><c-n>')
 
-s({ 'i', 'c' }, '<a-m>', '<esc><esc>', o)
-s('t', '<esc>', '<c-\\><c-n>', o)
-s('t', '<a-m>', '<c-\\><c-n>', o)
+s({ 'i', 'c' }, '<a-m>', '<esc><esc>')
+s('t', '<esc>', '<c-\\><c-n>')
+s('t', '<a-m>', '<c-\\><c-n>')
