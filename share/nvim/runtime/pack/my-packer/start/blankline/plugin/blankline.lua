ffe26d68adc3038934c3718592b1461268e81093
peter-lyr <llydrp.ldp@gmail.com>
2023-04-20 14:56:47 +0800

    blankline autoload twice

--- share/nvim/runtime/pack/my-packer/start/blankline/plugin/blankline.lua
+++ share/nvim/runtime/pack/my-packer/start/blankline/plugin/blankline.lua
@@ -9,9 +9,6 @@ g.blankline_lua = f['expand']('<sfile>')
 local blankline = function()
   if not g.blankline_loaded then
     g.blankline_loaded = 1
-    if g.blankline_cursormoved then
-      a.nvim_del_autocmd(g.blankline_cursormoved)
-    end
     sta, Do_blankline = pcall(require, 'do_blankline')
     if not sta then
       print(Do_blankline)
