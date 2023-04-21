eaab9416910e4db74265ec89829fd87c4aad0de6
peter-lyr <llydrp.ldp@gmail.com>
2023-04-21 11:16:07 +0800

    gitpush

--- share/nvim/runtime/pack/my-packer/start/gitpush/plugin/gitpush.lua
+++ share/nvim/runtime/pack/my-packer/start/gitpush/plugin/gitpush.lua
@@ -5,6 +5,7 @@ local s = vim.keymap.set
 
 local gitpush_loaded = nil
 local gitpush_cursormoved = nil
+local do_gitpush = nil
 
 local sta
 
@@ -14,16 +15,16 @@ local gitpush = function(params)
   if not gitpush_loaded then
     gitpush_loaded = 1
     a.nvim_del_autocmd(gitpush_cursormoved)
-    sta, Do_gitpush = pcall(require, 'do_gitpush')
+    sta, do_gitpush = pcall(require, 'do_gitpush')
     if not sta then
-      print(Do_gitpush)
+      print(do_gitpush)
       return
     end
   end
-  if not Do_gitpush then
+  if not do_gitpush then
     return
   end
-  Do_gitpush.run(params)
+  do_gitpush.run(params)
 end
 
 a.nvim_create_user_command('GitpusH', function(params)
@@ -36,6 +37,10 @@ gitpush_cursormoved = a.nvim_create_autocmd({ 'CursorMoved', 'FocusLost', 'Curso
   end,
 })
 
+-- local start_cmd = function(cmd)
+--   f['system'](string.format([[start cmd /c "%s" && pause]], cmd))
+-- end
+
 
 local opt = { silent = true }
 
@@ -45,4 +50,5 @@ s({ 'n', 'v' }, '<leader>g3', ':GitpusH just_push<cr>', opt)
 s({ 'n', 'v' }, '<leader>g4', ':GitpusH add_commit<cr>', opt)
 s({ 'n', 'v' }, '<leader>g5', ':GitpusH just_commit<cr>', opt)
 s({ 'n', 'v' }, '<leader>gI', ':GitpusH git_init<cr>', opt)
-s({ 'n', 'v' }, '<leader>g<f1>', ':!git log --all --graph --decorate --oneline && pause<cr>', opt)
+s({ 'n', 'v' }, '<leader>g<f1>', [[:silent exe '!start cmd /c "git log --all --graph --decorate --oneline" && pause'<cr>]], opt)
+-- s({ 'n', 'v' }, '<leader>g<f1>', function() start_cmd('git log --all --graph --decorate --oneline') end, opt)
