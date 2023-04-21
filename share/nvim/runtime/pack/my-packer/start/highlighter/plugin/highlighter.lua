f2a1df44d38fdfbd18d8fc6b7c28fea497452920
peter-lyr <llydrp.ldp@gmail.com>
2023-04-21 10:45:26 +0800

    highlighter err del autocmd

--- share/nvim/runtime/pack/my-packer/start/highlighter/plugin/highlighter.lua
+++ share/nvim/runtime/pack/my-packer/start/highlighter/plugin/highlighter.lua
@@ -5,7 +5,7 @@ local bufferclean_cursormoved
 
 bufferclean_cursormoved = a.nvim_create_autocmd({ 'CursorMoved', 'FocusLost', 'CursorHold' }, {
   callback = function()
-    a.nvim_create_autocmd(bufferclean_cursormoved)
+    a.nvim_del_autocmd(bufferclean_cursormoved)
     local sta, packadd = pcall(c, 'packadd vim-highlighter')
     if not sta then
       print(packadd)
