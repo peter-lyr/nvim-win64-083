a095b8625c25f115c928adf0924bf628e0920a94
peter-lyr <llydrp.ldp@gmail.com>
2023-04-21 14:45:55 +0800

    hop no vmap

--- share/nvim/runtime/pack/my-packer/start/hop/plugin/hop.lua
+++ share/nvim/runtime/pack/my-packer/start/hop/plugin/hop.lua
@@ -27,5 +27,5 @@ end, { nargs = '*', })
 
 local opt = { silent = true }
 
-s({'n', 'v'}, 's', ':HoP Char1<cr>', opt)
-s({'n', 'v'}, 'f', ':HoP Char2<cr>', opt)
+s('n', 's', ':HoP Char1<cr>', opt)
+s('n', 'f', ':HoP Char2<cr>', opt)
