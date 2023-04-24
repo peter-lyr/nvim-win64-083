4020809cf2b8e1d90c2a0509b08bee5db372c9a4
peter-lyr <llydrp.ldp@gmail.com>
2023-04-24 14:19:54 +0800

    mason cmd

--- share/nvim/runtime/my-neovim-data/mason/bin/pyright.cmd
+++ share/nvim/runtime/my-neovim-data/mason/bin/pyright.cmd
@@ -7,5 +7,5 @@ EXIT /b
 SETLOCAL
 CALL :find_dp0
 
-REM  endLocal & goto #_undefined_# 2>NUL || title %COMSPEC% & "D:\Desktop\nvim-win64-083\share\nvim\runtime\my-neovim-data\mason\packages\pyright\node_modules\.bin\pyright.cmd" %*
-endLocal & goto #_undefined_# 2>NUL || title %COMSPEC% & "C:\Users\llydr\Desktop\nvim-win64-083\share\nvim\runtime\my-neovim-data\mason\packages\pyright\node_modules\.bin\pyright.cmd" %*
+endLocal & goto #_undefined_# 2>NUL || title %COMSPEC% & "D:\Desktop\nvim-win64-083\share\nvim\runtime\my-neovim-data\mason\packages\pyright\node_modules\.bin\pyright.cmd" %*
+REM  endLocal & goto #_undefined_# 2>NUL || title %COMSPEC% & "C:\Users\llydr\Desktop\nvim-win64-083\share\nvim\runtime\my-neovim-data\mason\packages\pyright\node_modules\.bin\pyright.cmd" %*
