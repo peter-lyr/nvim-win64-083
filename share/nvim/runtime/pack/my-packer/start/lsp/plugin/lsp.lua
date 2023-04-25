e5dc51798e72c4889b2b70c16322697d4dfb161d
peter-lyr <llydrp.ldp@gmail.com>
2023-04-26 00:02:59 +0800

    lsp.lua

--- share/nvim/runtime/pack/my-packer/start/lsp/plugin/lsp.lua
+++ share/nvim/runtime/pack/my-packer/start/lsp/plugin/lsp.lua
@@ -1,21 +1,21 @@
 local a = vim.api
 
-local lsp_loaded = nil
-local lsp_cursormoved = nil
+local lsp_bufreadpre
 
-local lsp = function()
-  if not lsp_loaded then
-    lsp_loaded = 1
-    a.nvim_del_autocmd(lsp_cursormoved)
-    local sta, do_lsp = pcall(require, 'do_lsp')
-    if not sta then
-      print(do_lsp)
-    end
+local sta
+local do_lsp
+
+local lsp_init = function()
+  sta, do_lsp = pcall(require, 'do_lsp')
+  if not sta then
+    print(do_lsp)
   end
 end
 
-lsp_cursormoved = a.nvim_create_autocmd({ 'CursorMoved', 'FocusLost', 'CursorHold' }, {
+lsp_bufreadpre = a.nvim_create_autocmd({ 'BufReadPre' }, {
+  pattern = { '*.c', '*.h', '*.lua', '*.py' },
   callback = function()
-    lsp()
+    a.nvim_del_autocmd(lsp_bufreadpre)
+    lsp_init()
   end,
 })
