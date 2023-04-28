08a2a1d6e483d3e5db3f342baba138ec84b0d840
peter-lyr <llydrp.ldp@gmail.com>
2023-04-29 03:12:11 +0800

    telescope live grep

--- share/nvim/runtime/pack/my-packer/start/telescope/plugin/telescope.lua
+++ share/nvim/runtime/pack/my-packer/start/telescope/plugin/telescope.lua
@@ -55,9 +55,8 @@ s({ 'n', 'v' }, '<a-;>o', ':<c-u>TelescopE git_bcommits<cr>', opt)
 s({ 'n', 'v' }, '<a-;>h', ':<c-u>TelescopE git_branches<cr>', opt)
 s({ 'n', 'v' }, '<a-;>j', ':<c-u>TelescopE git_status previewer=false<cr>', opt)
 
--- s({ 'n', 'v' }, '<a-l>', ':<c-u>TelescopE live_grep previewer=false<cr>', opt)
-s({ 'n', 'v' }, '<a-l>', ':<c-u>TelescopE grep_string previewer=false shorten_path=true word_match=-w only_sort_text=true search= grep_open_files=true<cr>', opt)
-s({ 'n', 'v' }, '<a-i>', ':<c-u>TelescopE grep_string previewer=false<cr>', opt)
+s({ 'n', 'v' }, '<a-l>', ':<c-u>TelescopE live_grep<cr>', opt)
+s({ 'n', 'v' }, '<a-i>', ':<c-u>TelescopE grep_string shorten_path=true word_match=-w only_sort_text=true search= grep_open_files=true<cr>', opt)
 
 s({ 'n', 'v' }, '<a-q>', ':<c-u>TelescopE quickfix<cr>', opt)
 s({ 'n', 'v' }, '<a-Q>', ':<c-u>TelescopE quickfixhistory<cr>', opt)
