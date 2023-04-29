c715a8d0435b4de9a11edf7e04b6285eed12d34e
peter-lyr <llydrp.ldp@gmail.com>
2023-04-30 04:48:40 +0800

    blankline

--- share/nvim/runtime/pack/my-packer/start/blankline/plugin/blankline.lua
+++ share/nvim/runtime/pack/my-packer/start/blankline/plugin/blankline.lua
@@ -1,27 +1,22 @@
 local a = vim.api
-local g = vim.g
 
 local blankline_loaded = nil
-
-local sta
 
 local blankline = function()
   if not blankline_loaded then
     blankline_loaded = 1
-    sta, Do_blankline = pcall(require, 'do_blankline')
+    local sta, do_blankline = pcall(require, 'do_blankline')
     if not sta then
-      print(Do_blankline)
+      print(do_blankline)
       return
     end
   end
-  if not Do_blankline then
-    return
-  end
-  Do_blankline.run()
 end
 
-g.blankline_cursormoved = a.nvim_create_autocmd({ 'CursorMoved', 'FocusLost', 'CursorHold' }, {
+local blankline_cursormoved
+blankline_cursormoved = a.nvim_create_autocmd({ 'CursorMoved', 'FocusLost', 'CursorHold' }, {
   callback = function()
+    a.nvim_del_autocmd(blankline_cursormoved)
     blankline()
   end,
 })
