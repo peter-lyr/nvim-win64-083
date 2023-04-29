49d6fd127693ab1b02ce8157a3fd37a4798b4c5d
peter-lyr <llydrp.ldp@gmail.com>
2023-04-30 01:19:28 +0800

    test_get_process_pid_by_title.c void get_process_pid_by_title(char *title, DWORD *pid);

--- share/nvim/runtime/pack/my-packer/start/process/autoload/test_get_process_pid_by_title.c
+++ share/nvim/runtime/pack/my-packer/start/process/autoload/test_get_process_pid_by_title.c
@@ -1,11 +1,17 @@
-#include <windows.h>
 #include <stdio.h>
+#include <windows.h>
 
-int main()
-{
-    HWND hwnd = FindWindow(NULL, "nvim-win64-083");
-    DWORD pid;
-    GetWindowThreadProcessId(hwnd, &pid);
-    printf("PID: %ld\n", pid);
+void get_process_pid_by_title(char *title, DWORD *pid) {
+  HWND hwnd = FindWindow(NULL, title);
+  GetWindowThreadProcessId(hwnd, pid);
+}
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
     return 0;
+  }
+  DWORD pid;
+  get_process_pid_by_title(argv[1], &pid);
+  printf("PID: %ld\n", pid);
+  return 0;
 }
