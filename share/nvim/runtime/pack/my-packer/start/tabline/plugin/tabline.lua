f95c7c06f0d52dbca5f95069e7c9c9dbcb5de154
peter-lyr <llydrp.ldp@gmail.com>
2023-04-30 00:56:16 +0800

    tabline winleave

--- share/nvim/runtime/pack/my-packer/start/tabline/plugin/tabline.lua
+++ share/nvim/runtime/pack/my-packer/start/tabline/plugin/tabline.lua
@@ -11,10 +11,18 @@ set_tabline = a.nvim_create_autocmd({ 'CursorMoved' }, {
   callback = function()
     a.nvim_del_autocmd(set_tabline)
     c([[set tabline=%!tabline#tabline()]])
-    local sta, do_tabline = pcall(require, 'do_tabline')
-    if not sta then
-      print('no do_tabline')
-      return
+  end,
+})
+
+local sta, do_tabline
+a.nvim_create_autocmd({ 'WinLeave' }, {
+  callback = function()
+    if not do_tabline then
+      sta, do_tabline = pcall(require, 'do_tabline')
+      if not sta then
+        print('no do_tabline')
+        return
+      end
     end
     f['timer_start'](100, do_tabline.update_title_string)
   end,
