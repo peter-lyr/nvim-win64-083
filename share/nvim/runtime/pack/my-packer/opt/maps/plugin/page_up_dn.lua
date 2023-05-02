4a7169983a570f776b6b7c104af1abb19efe07a9
peter-lyr <llydrp.ldp@gmail.com>
2023-05-02 17:49:10 +0800

    page_up_dn.lua cancel

--- share/nvim/runtime/pack/my-packer/opt/maps/plugin/page_up_dn.lua
+++ share/nvim/runtime/pack/my-packer/opt/maps/plugin/page_up_dn.lua
@@ -1,7 +1,7 @@
-local s = vim.keymap.set
-local o = { silent = true }
-
-s({ 'n', 'v' }, '<c-d>', '<c-u>', o)
-s({ 'n', 'v' }, '<c-f>', '<c-d>', o)
-s({ 'n', 'v' }, '<c-u>', '<c-b>', o)
-s({ 'n', 'v' }, '<c-b>', '<c-f>', o)
+-- local s = vim.keymap.set
+-- local o = { silent = true }
+--
+-- s({ 'n', 'v' }, '<c-d>', '<c-u>', o)
+-- s({ 'n', 'v' }, '<c-f>', '<c-d>', o)
+-- s({ 'n', 'v' }, '<c-u>', '<c-b>', o)
+-- s({ 'n', 'v' }, '<c-b>', '<c-f>', o)
