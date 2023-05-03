974517fccb56eca384471209ce75c89e67a81247
peter-lyr <llydrp.ldp@gmail.com>
2023-05-03 17:30:28 +0800

    telescope no my live_grep

--- share/nvim/runtime/pack/my-packer/start/telescope/lua/do_telescope.lua
+++ share/nvim/runtime/pack/my-packer/start/telescope/lua/do_telescope.lua
@@ -275,18 +275,18 @@ M.grep_string = function()
   end)
 end
 
-M.live_grep = function()
-  local cwd = f['getcwd']()
-  local sub_dirs = get_sub_dirs(cwd)
-  -- show_array(sub_dirs)
-  vim.ui.select(sub_dirs, { prompt = 'select one of them' }, function(_, idx)
-    -- print(choice, idx)
-    local dir = sub_dirs[idx]
-    print(dir)
-    local cmd = 'Telescope live_grep shorten_path=true word_match=-w only_sort_text=true search= search_dirs=' .. dir
-    c(cmd)
-  end)
-end
+-- M.live_grep = function()
+--   local cwd = f['getcwd']()
+--   local sub_dirs = get_sub_dirs(cwd)
+--   -- show_array(sub_dirs)
+--   vim.ui.select(sub_dirs, { prompt = 'select one of them' }, function(_, idx)
+--     -- print(choice, idx)
+--     local dir = sub_dirs[idx]
+--     print(dir)
+--     local cmd = 'Telescope live_grep shorten_path=true word_match=-w only_sort_text=true search= search_dirs=' .. dir
+--     c(cmd)
+--   end)
+-- end
 
 local projectroots = {}
 
@@ -340,8 +340,8 @@ M.run = function(params)
   if params[1] == 'my' then
     if params[2] == 'grep_string' then
       M.grep_string()
-    elseif params[2] == 'live_grep' then
-      M.live_grep()
+    -- elseif params[2] == 'live_grep' then
+    --   M.live_grep()
     elseif params[2] == 'projectsbuffers' then
       M.projectsbuffers()
     elseif params[2] == 'projectsbuffersall' then
--- share/nvim/runtime/pack/my-packer/start/telescope/plugin/telescope.lua
+++ share/nvim/runtime/pack/my-packer/start/telescope/plugin/telescope.lua
@@ -58,7 +58,7 @@ s({ 'n', 'v' }, '<a-;>h', ':<c-u>TelescopE git_branches<cr>', opt)
 s({ 'n', 'v' }, '<a-;>j', ':<c-u>TelescopE git_status previewer=false<cr>', opt)
 
 s({ 'n', 'v' }, '<a-l>', ':<c-u>TelescopE live_grep shorten_path=true word_match=-w only_sort_text=true search= grep_open_files=true<cr>', opt)
-s({ 'n', 'v' }, '<a-L>', ':<c-u>TelescopE my live_grep<cr>', opt)
+s({ 'n', 'v' }, '<a-L>', ':<c-u>TelescopE live_grep<cr>', opt)
 s({ 'n', 'v' }, '<a-i>', ':<c-u>TelescopE grep_string shorten_path=true word_match=-w only_sort_text=true search= grep_open_files=true<cr>', opt)
 s({ 'n', 'v' }, '<a-I>', ':<c-u>TelescopE my grep_string<cr>', opt)
 
