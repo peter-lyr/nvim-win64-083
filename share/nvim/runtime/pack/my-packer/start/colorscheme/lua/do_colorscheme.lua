eba5d86f13eb015295e4a3d894b521ebf15415c9
peter-lyr <llydrp.ldp@gmail.com>
2023-05-07 12:37:48 +0800

    leader bw change colorscheme

--- share/nvim/runtime/pack/my-packer/start/colorscheme/lua/do_colorscheme.lua
+++ share/nvim/runtime/pack/my-packer/start/colorscheme/lua/do_colorscheme.lua
@@ -13,19 +13,31 @@ for _, v in pairs(vim.fn.getcompletion("", "color")) do
   table.insert(ColorSchemes, v)
 end
 
-local changecolorscheme = function()
-  local color
-  for _=1, 5 do
-    color = ColorSchemes[math.random(#ColorSchemes)]
-    if not string.match(color, 'light') then
-      break
+local colors = {}
+
+local changecolorscheme = function(force)
+  local cwd = string.lower(string.gsub(vim.loop.cwd(), '\\', '/'))
+  if not vim.tbl_contains(vim.tbl_keys(colors), cwd) or force == true then
+    local color
+    for _=1, 5 do
+      color = ColorSchemes[math.random(#ColorSchemes)]
+      if not string.match(color, 'light') then
+        break
+      end
     end
+    c(string.format([[call feedkeys(":\<c-u>colorscheme %s\<cr>")]], color))
+    colors[cwd] = color
+  else
+    c(string.format([[call feedkeys(":\<c-u>colorscheme %s\<cr>")]], colors[cwd]))
   end
-  c(string.format([[call feedkeys(":\<c-u>colorscheme %s\<cr>")]], color))
 end
 
 a.nvim_create_autocmd({ 'TabEnter', }, {
   callback = function()
-    changecolorscheme()
+    changecolorscheme(false)
   end,
 })
+
+local s = vim.keymap.set
+local opt = { silent = true }
+s({ 'n', 'v', 'i' }, '<leader>bw', function() changecolorscheme(true) end, opt)
