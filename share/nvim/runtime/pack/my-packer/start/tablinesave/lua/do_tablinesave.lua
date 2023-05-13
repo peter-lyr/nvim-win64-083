9d5c7d26ad6908cdc38c50adb1fad9e9876cd6b9
peter-lyr <llydrp.ldp@gmail.com>
2023-05-13 21:49:16 +0800

    leader bU bT

--- share/nvim/runtime/pack/my-packer/start/tablinesave/lua/do_tablinesave.lua
+++ share/nvim/runtime/pack/my-packer/start/tablinesave/lua/do_tablinesave.lua
@@ -1,5 +1,6 @@
 local f = vim.fn
 local a = vim.api
+local c = vim.cmd
 
 local M = {}
 
@@ -25,17 +26,15 @@ if not session:exists() then
   session:touch()
 end
 
-local D = {}
+local newcnt = 0
 
-M.savesession = function()
-  -- for _, v in ipairs(session:readlines()) do
-  --   print("v:", v)
-  -- end
+M.getnew = function()
   local fname
   local fpath
   local cwd
   local branch
-  D = {}
+  local new = {}
+  newcnt = 0
   for _, bufnr in ipairs(a.nvim_list_bufs()) do
     if f['buflisted'](bufnr) == 0 or a.nvim_buf_is_loaded(bufnr) == false then
       goto continue
@@ -60,21 +59,83 @@ M.savesession = function()
     else
       branch = f['gitbranch#name']()
     end
-    if not vim.tbl_contains(vim.tbl_keys(D), cwd) then
+    newcnt = newcnt + 1
+    if not vim.tbl_contains(vim.tbl_keys(new), cwd) then
       local f1 = { fname }
       local b1 = {}
       b1[branch] = f1
-      D[cwd] = b1
+      new[cwd] = b1
     else
-      if not vim.tbl_contains(vim.tbl_keys(D[cwd]), branch) then
-        D[cwd][branch] = { fname }
+      if not vim.tbl_contains(vim.tbl_keys(new[cwd]), branch) then
+        new[cwd][branch] = { fname }
       else
-        table.insert(D[cwd][branch], fname)
+        table.insert(new[cwd][branch], fname)
       end
     end
     ::continue::
   end
-  print("D:", vim.inspect(D))
+  return new
+end
+
+M.savesession = function()
+  local old = loadstring('return ' .. session:read())()
+  local new = M.getnew()
+  for cwd, v1 in pairs(old) do
+    if not vim.tbl_contains(vim.tbl_keys(new), cwd) then
+      new[cwd] = v1
+    else
+      for branch, fnames in pairs(v1) do
+        if not vim.tbl_contains(vim.tbl_keys(new[cwd]), branch) then
+          new[cwd][branch] = fnames
+        -- else
+        --   for _, fname in ipairs(fnames) do
+        --     if not vim.tbl_contains(new[cwd][branch], fname) then
+        --       table.insert(new[cwd][branch], fname)
+        --       print("fname:", vim.inspect(fname))
+        --     end
+        --   end
+        end
+      end
+    end
+  end
+  local newcontent = tostring(vim.inspect(new))
+  session:write(newcontent, 'w')
+  print('save ' .. #vim.tbl_keys(new) .. ' cwd, ' .. newcnt .. ' opened files')
+end
+
+local bwande = function(cwd, fnames)
+  cwd = f['tolower'](cwd)
+  for _, bufnr in ipairs(a.nvim_list_bufs()) do
+    local fname = string.gsub(a.nvim_buf_get_name(bufnr), '\\', '/')
+    local curcwd = f['projectroot#get'](fname)
+    curcwd = f['tolower'](curcwd)
+    if curcwd == cwd then
+      if not vim.tbl_contains(fnames, fname) then
+        c('bw! ' .. fname)
+      end
+    end
+  end
+  for _, fname in ipairs(fnames) do
+    c('e ' .. fname)
+  end
+  f['tabline#restorehiddenprojects']()
+end
+
+M.loadsession = function()
+  local cwds = loadstring('return ' .. session:read())()
+  vim.ui.select(vim.tbl_keys(cwds), { prompt = 'cwd' }, function(cwd, _)
+    local branches = cwds[cwd]
+    if #vim.tbl_keys(branches) == 1 then
+      for _, fnames in pairs(branches) do
+        bwande(cwd, fnames)
+      end
+    else
+      vim.ui.select(vim.tbl_keys(branches), { prompt = 'branch' }, function(branch, _)
+        local fnames = branches[branch]
+        bwande(cwd, fnames)
+      end)
+    end
+  end)
 end
 
 M.run = function(params)
@@ -84,8 +145,10 @@ M.run = function(params)
 
   if #params == 1 then
     local param = params[1]
-    if param == 'all' then
+    if param == 'save' then
       M.savesession()
+    elseif param == 'load' then
+      M.loadsession()
     end
   end
 end
