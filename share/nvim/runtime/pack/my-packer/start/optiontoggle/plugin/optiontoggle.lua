fd77c1206078135a80a6ed2034dd890edfda92a8
peter-lyr <llydrp.ldp@gmail.com>
2023-05-13 22:16:00 +0800

    leader tw

--- share/nvim/runtime/pack/my-packer/start/optiontoggle/plugin/optiontoggle.lua
+++ share/nvim/runtime/pack/my-packer/start/optiontoggle/plugin/optiontoggle.lua
@@ -38,4 +38,4 @@ end, { nargs = '*', })
 
 local s = vim.keymap.set
 local opt = { silent = true }
-s({ 'n', 'v' }, '<leader>xw', ':<c-u>OptiontogglE wrap<cr>', opt)
+s({ 'n', 'v' }, '<leader>tw', ':<c-u>OptiontogglE wrap<cr>', opt)
