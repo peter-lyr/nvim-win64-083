7828bda7ff426d413548f83b4e94704db4d47ff6
peter-lyr <llydrp.ldp@gmail.com>
2023-05-14 11:22:30 +0800

    bcomp not exists write temp

--- share/nvim/runtime/pack/my-packer/start/bcomp/lua/do_bcomp.lua
+++ share/nvim/runtime/pack/my-packer/start/bcomp/lua/do_bcomp.lua
@@ -1,4 +1,5 @@
 local a = vim.api
+local f = vim.fn
 local c = vim.cmd
 
 local M = {}
@@ -17,24 +18,23 @@ M.run = function(params)
   if #params == 1 then
     local param = params[1]
     local fpath = Path:new(a.nvim_buf_get_name(0))
-    if fpath:exists() then
-      if param == '1' then
+    if not fpath:exists() then
+      local tempfname1 = string.gsub(fpath.filename, '\\', '/')
+      local tempfname2 = string.match(tempfname1, '.*/([^/]+)')
+      local temppath = Path:new(f['expand']('$TEMP'))
+      fpath = temppath:joinpath(tempfname2)
+      fpath:write(table.concat(f['getline'](1, f['line']('$')), '\n'), 'w')
+    end
+    if param == '1' then
+      file1 = fpath.filename
+    elseif param == '2' then
+      if #file1 == 0 then
         file1 = fpath.filename
-      elseif param == '2' then
-        if #file1 == 0 then
-          file1 = fpath.filename
-          c('echo "bcomp file1: ' .. file1 .. '"')
-        else
-          file2 = fpath.filename
-          c('echo "bcomp file2: ' .. file2 .. '"')
-          c(string.format([[silent !start /b /min cmd /c "bcomp "%s" "%s""]], file1, file2))
-        end
-      end
-    else
-      if param == '1' then
-        file1 = ''
-      elseif param == '2' then
-        file2 = ''
+        c('echo "bcomp file1: ' .. file1 .. '"')
+      else
+        file2 = fpath.filename
+        c('echo "bcomp file2: ' .. file2 .. '"')
+        c(string.format([[silent !start /b /min cmd /c "bcomp "%s" "%s""]], file1, file2))
       end
     end
     if param == '3' then
