1caec9b97fe9048cbff3d4a8418680391cee070a
peter-lyr <llydrp.ldp@gmail.com>
2023-05-16 10:52:23 +0800

    changecolorschemedefault()

--- share/nvim/runtime/pack/my-packer/start/colorscheme/lua/do_colorscheme.lua
+++ share/nvim/runtime/pack/my-packer/start/colorscheme/lua/do_colorscheme.lua
@@ -68,4 +68,4 @@ end)
 local s = vim.keymap.set
 local opt = { silent = true }
 s({ 'n', 'v' }, '<leader>bw', function() changecolorscheme(true) end, opt)
-s({ 'n', 'v' }, '<leader>bW', function() changecolorschemedefault(true) end, opt)
+s({ 'n', 'v' }, '<leader>bW', function() changecolorschemedefault() end, opt)
